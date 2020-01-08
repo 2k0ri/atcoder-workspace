cd575d8dbe68e41d5ba52a9d3e245a2d95287a54
2k0ri <esc13245@gmail.com>
2020-01-08 23:53:21 +0900

    abc069abcd

---
+++ abc069/B/main.cpp
@@ -0,0 +1,35 @@
+// clang-format off
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for(ll i=0, i##_len=(n); i<i##_len; ++i)
+#define rep1(i,n) for(ll i=1, i##_len=(n); i<=i##_len; ++i)
+#define rrep(i,n) for(ll i=n-1; i>=0; --i)
+#define rrep1(i,n) for(ll i=n; i>0; --i)
+#define all(x) (x).begin(),(x).end()
+#define SORT(x) sort(all(x))
+#define SORT_DESC(x) sort(all(x), greater<>())
+#define pb push_back
+#define mp make_pair
+#define coutl cout<<fixed<<setprecision(10)
+#define ANS(ans) cout<<ans<<endl;
+#define INF 1e9;
+#define LINF 1e18;
+template<class T>bool chmax(T &a, const T &b){if (a<b){a=b;return 1;}return 0;}
+template<class T>bool chmin(T &a, const T &b){if (b<a){a=b;return 1;}return 0;}
+// clang-format on
+
+void solve(std::string s) {
+  ll n = s.size();
+  ANS(s[0] << n - 2 << s[n - 1]);
+}
+
+// clang-format off
+int main() {
+  std::string s;
+  std::cin >> s;
+  solve(s);
+  return 0;
+}
---
+++ abc069/C/main.cpp
@@ -0,0 +1,53 @@
+// clang-format off
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for(ll i=0, i##_len=(n); i<i##_len; ++i)
+#define rep1(i,n) for(ll i=1, i##_len=(n); i<=i##_len; ++i)
+#define rrep(i,n) for(ll i=n-1; i>=0; --i)
+#define rrep1(i,n) for(ll i=n; i>0; --i)
+#define all(x) (x).begin(),(x).end()
+#define SORT(x) sort(all(x))
+#define SORT_DESC(x) sort(all(x), greater<>())
+#define pb push_back
+#define mp make_pair
+#define coutl cout<<fixed<<setprecision(10)
+#define ANS(ans) cout<<ans<<endl;
+#define INF 1e9;
+#define LINF 1e18;
+template<class T>bool chmax(T &a, const T &b){if (a<b){a=b;return 1;}return 0;}
+template<class T>bool chmin(T &a, const T &b){if (b<a){a=b;return 1;}return 0;}
+const string YES = "Yes";
+const string NO = "No";
+// clang-format on
+
+void solve(long long N, std::vector<long long> a) {
+  ll b1 = 0, b2 = 0, b4 = 0;
+  rep(i, N) {
+    if (a[i] % 2 != 0)
+      b1++;
+    else if (a[i] % 4 != 0)
+      b2++;
+    else
+      b4++;
+  }
+  if (b2 == 0) {
+    ANS((b1 <= b4 + 1 ? YES : NO));
+  } else {
+    ANS((b1 <= b4 ? YES : NO));
+  }
+}
+
+// clang-format off
+int main() {
+  long long N;
+  scanf("%lld",&N);
+  std::vector<long long> a(N);
+  for(int i = 0 ; i < N ; i++){
+    scanf("%lld",&a[i]);
+  }
+  solve(N, std::move(a));
+  return 0;
+}
